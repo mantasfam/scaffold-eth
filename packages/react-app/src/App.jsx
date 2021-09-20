53f47446a1cb975fd309a530f19288051bda8a67
Mantas Stašauskas <mantas487@gmail.com>
2021-09-21 00:15:24 +0300

    Header is done

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -1,52 +1,101 @@
-/*import { StaticJsonRpcProvider, Web3Provider } from "@ethersproject/providers";
-import { formatEther, parseEther } from "@ethersproject/units";
+import { /*StaticJsonRpcProvider,*/ Web3Provider } from "@ethersproject/providers";
+import { formatEther /*, parseEther */ } from "@ethersproject/units";
 import { BigNumber } from "@ethersproject/bignumber";
 import WalletConnectProvider from "@walletconnect/web3-provider";
-import { Alert, Button, Card, Col, Input, List, Menu, Row } from "antd";
-import "antd/dist/antd.css";
-import { useUserAddress } from "eth-hooks";*/
-import React /*, { useCallback, useEffect, useState }*/ from "react";
+/*import { Alert, Button, Card, Col, Input, List, Menu, Row } from "antd";
+import "antd/dist/antd.css";*/
+import { useUserAddress } from "eth-hooks";
+import React, { useCallback, useEffect, useState } from "react";
 /*import ReactJson from "react-json-view";
-import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
-import Web3Modal from "web3modal";*/
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";*/
+import Web3Modal from "web3modal";
 import "./App.css";
-/*import {
-  Account,
+import {
+  /*Account,
   Address,
   AddressInput,
   Contract,
   Faucet,
-  GasGauge,
+  GasGauge,*/
   Header,
-  Ramp,
+  /*Ramp,
   ThemeSwitch,
   Sell,
   Mint,
   LazyMint,
-  RaribleItemIndexer,
+  RaribleItemIndexer,*/
 } from "./components";
-import { DAI_ABI, DAI_ADDRESS, INFURA_ID, NETWORK, NETWORKS } from "./constants";
-import { Transactor } from "./helpers";
+import { /*DAI_ABI, DAI_ADDRESS,*/ INFURA_ID /*NETWORK, NETWORKS*/ } from "./constants";
+//import { Transactor } from "./helpers";
 import {
   useBalance,
-  useContractLoader,
+  /*useContractLoader,
   useContractReader,
   useEventListener,
   useExchangePrice,
   useExternalContractLoader,
   useGasPrice,
   useOnBlock,
-  useUserProvider,
+  useUserProvider,*/
 } from "./hooks";
-import { matchSellOrder, prepareMatchingOrder } from "./rarible/createOrders";*/
+//import { matchSellOrder, prepareMatchingOrder } from "./rarible/createOrders";
 
 console.log("STARTING PAGE 0....");
 
+/*
+  Web3 modal helps us "connect" external wallets:
+*/
+const web3Modal = new Web3Modal({
+  // network: "mainnet", // optional
+  cacheProvider: true, // optional
+  providerOptions: {
+    walletconnect: {
+      package: WalletConnectProvider, // required
+      options: {
+        infuraId: INFURA_ID,
+      },
+    },
+  },
+});
+
 function App(props) {
   console.log("STARTING PAGE 1....");
+
+  const [injectedProvider, setInjectedProvider] = useState();
+  const loadWeb3Modal = useCallback(async () => {
+    const provider = await web3Modal.connect();
+    setInjectedProvider(new Web3Provider(provider));
+  }, [setInjectedProvider]);
+  useEffect(() => {
+    if (web3Modal.cachedProvider) {
+      loadWeb3Modal();
+    }
+  }, [loadWeb3Modal]);
+
+  const metamaskAddressLocal = useUserAddress(injectedProvider);
+  const metamaskAddressBalanceWeiLocal = useBalance(injectedProvider, metamaskAddressLocal);
+
+  /* hook panaudojimas setState
+  const [metamaskAddress, setMetamaskAddress] = useState();
+  useEffect(() => {
+    setMetamaskAddress(metamaskAddressLocal);
+  }, [metamaskAddressLocal]);
+  const [metamaskAddressBalanceWei, setMetamaskAddressBalanceWei] = useState();
+  useEffect(() => {
+    setMetamaskAddressBalanceWei(metamaskAddressBalanceWeiLocal);
+  }, [metamaskAddressBalanceWeiLocal]);*/
+
   return (
     <div className="App">
-      <h1>APP ORIG</h1>
+      <Header />
+
+      <h1>APP</h1>
+
+      {/* 👨‍💼 Your account is in the top right with a wallet at connect options */}
+      <div style={{ position: "fixed", textAlign: "right", right: 0, top: 0, padding: 10 }}>
+        <div> Metamask address: {metamaskAddressLocal}</div>
+        <div> Metamask address balance: {formatEther(metamaskAddressBalanceWeiLocal || BigNumber.from("0"))} eth</div>
+      </div>
     </div>
   );
 }
--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -2,15 +2,10 @@ import { PageHeader } from "antd";
 import React from "react";
 
 // displays a page header
-
 export default function Header() {
   return (
-    <a href="https://github.com/austintgriffith/scaffold-eth" target="_blank" rel="noopener noreferrer">
-      <PageHeader
-        title="🏗 scaffold-eth"
-        subTitle="🖼 Rarible Boost"
-        style={{ cursor: "pointer" }}
-      />
-    </a>
+    <div style={{ backgroundColor: "powderblue" }}>
+      <PageHeader title="🏗 NFT" subTitle="marketplace" style={{ cursor: "pointer" }} />
+    </div>
   );
 }
