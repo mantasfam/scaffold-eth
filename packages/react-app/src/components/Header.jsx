d067ce8b8d9164b88a581278d0a023585af33ca9
Mantas Stašauskas <mantas487@gmail.com>
2021-09-22 11:48:34 +0300

    Header is fully rendered in Header component

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -1,11 +1,19 @@
 import { PageHeader } from "antd";
 import React from "react";
+import { BigNumber } from "@ethersproject/bignumber";
+import { formatEther /*, parseEther */ } from "@ethersproject/units";
 
 // displays a page header
-export default function Header() {
+export default function Header({ metamaskNetworkName, metamaskAddress, metamaskAddressBalanceWei }) {
   return (
     <div style={{ backgroundColor: "powderblue" }}>
       <PageHeader title="🏗 NFT" subTitle="marketplace" style={{ cursor: "pointer" }} />
+      {/* 👨‍💼 Your account is in the top right with a wallet at connect options */}
+      <div style={{ position: "fixed", textAlign: "right", right: 0, top: 0, padding: 10 }}>
+        <div> Metamask network name: {metamaskNetworkName}</div>
+        <div> Metamask address: {metamaskAddress}</div>
+        <div> Metamask address balance: {formatEther(metamaskAddressBalanceWei || BigNumber.from("0"))} eth</div>
+      </div>
     </div>
   );
 }
